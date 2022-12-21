59e8e991a2379bb42e354f9980c6f56ea7ef0004
JoaogustavoKoche <joaogustavokoche@gmail.com>
2022-12-20 22:33:05 -0300

    add json with some data and fix preview home items

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 
-import { FirstSection, InputDiv, InvButton, BasketBar } from "./styles";
+import {Container, FirstSection, InputDiv, InvButton, BasketBar, SecondSection} from "./styles";
 import Input from '../../components/input/index';
 import Imgs from "../../components/img";
 import { useLocation } from "react-router-dom";
@@ -9,6 +9,15 @@ import icon from '../../assets/search.svg';
 import filter from '../../assets/filter.svg';
 import menuicon from '../../assets/menuIcon.svg';
 import basket from '../../assets/basket.svg';
+
+import text from '../../teste-data/quinoa.svg';
+import additem from '../../assets/addItem.svg'
+
+import fav from '../../assets/favorite.svg'
+
+
+import { Text, ContainerCards } from "./styles";
+
 
 
 export function Home(){
@@ -26,40 +35,80 @@ export function Home(){
         backgroundSize: '25px',
     }; 
 
+   
+   /*  const nameFruit = "fruta";
+    const price = 2000;
+  /*   const image = '../../teste-data/honeyLime.svg'; */  
 
     return(
-        <FirstSection>
-            <BasketBar>
-                <InvButton>
-                    <Imgs 
-                        src={menuicon}
-                        width={'35px'}
-                    />
-                </InvButton>
-                <InvButton
-                    style={{display: 'grid', justifyItems: 'center'}}
-                >
-                    <Imgs 
-                        src={basket}
-                        width={'35px'}
-                    />
-                    My basket
-                </InvButton>
-            </BasketBar>
-            <h1>  Hello {name} What fruit salad combo do you want today?</h1>          
-            <InputDiv>
-                    <Input 
-                        placeholder="Search for fruit salad combos"
-                        style={divStyle}
-                    />
+        
+        <Container>
+            <FirstSection>
+                <BasketBar>
                     <InvButton>
                         <Imgs 
-                            src={filter} 
-                            alt="Filter"
+                            src={menuicon}
                             width={'35px'}
                         />
                     </InvButton>
-            </InputDiv>
-        </FirstSection>
+                    <InvButton
+                        style={{display: 'grid', justifyItems: 'center'}}
+                    >
+                        <Imgs 
+                            src={basket}
+                            width={'35px'}
+                        />
+                        My basket
+                    </InvButton>
+                    
+                </BasketBar>
+                <h1> Hello {name} What fruit salad combo do you want today?</h1>          
+                <InputDiv>
+                        <Input 
+                            placeholder="Search for fruit salad combos"
+                            style={divStyle}
+                        />
+                        <InvButton>
+                            <Imgs 
+                                src={filter} 
+                                alt="Filter"
+                                width={'35px'}
+                            />
+                        </InvButton>
+                </InputDiv>
+            </FirstSection>
+
+
+
+        {/* usando apenas para ilustrar por hora */}
+        
+            <SecondSection>
+                 <Text>Recommended Combo</Text>
+                 <ContainerCards>
+                    <div className="containerImgs">
+                        <Imgs src={fav}
+                            width='20px'
+
+                        />
+                        <Imgs src={text}
+                            width='135px'
+
+                        />
+                    </div>
+
+                    
+                   
+                    <h1>Honey lime combo</h1>
+                    <div>
+                        <h3>&#x20A6; 2,000</h3>
+                        <Imgs src={additem}
+                            width='30px'
+                            height='none'
+                        />
+                    </div>
+                    
+                </ContainerCards>   
+            </SecondSection>
+        </Container>
     );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+
+    display: grid;
+`;
 
 export const FirstSection = styled.div`
 
@@ -26,7 +30,6 @@ export const FirstSection = styled.div`
     
 `;
 
-
 export const InputDiv = styled.div`
 
     display: flex;
@@ -44,10 +47,78 @@ export const BasketBar = styled.div`
     align-items: center;
 `;
 
-
 export const InvButton = styled.button`
 
     border-style: none;
     background: transparent;
 `;
+
+
+
+
+export const SecondSection = styled.div`
+
+    
+`;
+
+export const Text = styled.h1`
+
+`;
+
+export const ContainerCards = styled.div`
+
+    background-color: #FEF0F0;
+    border-bottom-left-radius: 20px;
+    border-bottom-right-radius: 20px;
+    border-top-left-radius: 20px;
+    border-top-right-radius: 20px;
+    margin: 30px;
+
+    display: inline-grid;
+    h1{
+        font-family: 'Brandon Grotesque';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 23px;
+        /* identical to box height */
+
+        display: flex;
+        align-items: flex-end;
+        letter-spacing: -0.01em;
+
+        color: #27214D;
+    }
+
+
+    h3{
+        font-family: 'Brandon Grotesque';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 20px;
+        /* identical to box height */
+
+        display: flex;
+        align-items: flex-end;
+        letter-spacing: -0.01em;
+
+        color: #F08626;
+
+    }
+
+    div.containerImgs{
+        display: inline-flex;
+        align-items: flex-end;
+        flex-direction: column;
+        padding: 20px;
+    }
+
+    div{
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+    }
+`;
+
 
